b64caf67338207a1d5024c6a7bd34db7f4962708
nams-github <99600280+nams-github@users.noreply.github.com>
2022-03-14 23:30:43 +0530

    Add files via upload
    Task3

--- automation.sh
+++ automation.sh
@@ -45,6 +45,33 @@ filename=$(ls -lrt | tail -1 | awk '{ print $NF }')
 cp $filename /tmp
 aws s3 cp /tmp/$filename s3://$s3bucket
 
+str1=$(echo $filename | awk -F- '{print $4F-$NF}')
+echo $str1
 
 
 
+
+cd /var/www/html
+if [ $(ls | grep inventory.html | wc -l) = 0 ]
+then
+        echo "inventory.html file is not present. So creating it"
+        echo -e "LogType\t\tDateCreated\t\tType\tSize" >> inventory.html
+fi
+
+size=$(stat --format %s /tmp/$filename)
+
+echo $size
+printf 'httpd-logs\t(%s)\ttar\t%s \n' "$str1" "$size" >> inventory.html
+
+
+
+if  [ $(crontab -u root -l | grep "0 10" | wc -l) = 0 ]
+then
+        echo "no cron job is defined for the user"
+
+        sudo crontab -l > /etc/cron.d/automation
+        sudo echo "0 10 * * * sudo /root/Automation_Project/automation.sh" >> /etc/cron.d/automation
+        sudo crontab /etc/cron.d/automation
+fi
+
+root@ip-172-31-89-93:~#
